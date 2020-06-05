0586445f70752df1d8a3f7a50f1a96b6a27ac995
MathieuBoe <mathieuboe60@gmail.com>
2020-06-05 12:23:57 +0200

    Exercice 7 complet

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$title = 'Exercice7';
+$isOK = False
+?>
+<? include 'header.php';?>
+<?php
+if ($isOK== False)
+
+{
+    echo "C'est pas bon !!!";
+}
+else 
+{
+    echo "C'est bon !!!";
+}
+?>
+<?include 'footer.php';?>
